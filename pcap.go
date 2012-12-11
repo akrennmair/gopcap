9e88f61c2a67a26dba2380254cef9c0de45f01ac
Graeme Connell <gconnell@google.com>
2012-12-10 18:25:49 -0700

    Modify packet capture a bit to for speed.
    Used benchmark to test this out.
    
    Old:
    (/tmp/bench is tools/benchmark with the old code)
    gconnell@zombie-test1.mtv:/$ /tmp/bench -file /tmp/foo
    Took 3.334896s to process 1000000 packets, 3.334us per packet, 0 nil packets
    gconnell@zombie-test1.mtv:/$ /tmp/bench -file /tmp/foo
    Took 3.329675s to process 1000000 packets, 3.329us per packet, 0 nil packets
    
    New:
    (/tmp/bench2 is tools/benchmark with the new code)
    gconnell@zombie-test1.mtv:/$ /tmp/bench2 -file /tmp/foo
    Took 1.588695s to process 1000000 packets, 1.588us per packet, 0 nil packets
    gconnell@zombie-test1.mtv:/$ /tmp/bench2 -file /tmp/foo
    Took 1.580306s to process 1000000 packets, 1.58us per packet, 0 nil packets
    
    About a 50% reduction in the initial packet copy.  This seemed to work pretty
    repeatably.

--- pcap.go
+++ pcap.go
@@ -93,15 +93,13 @@ func Openoffline(file string) (handle *Pcap, err error) {
 }
 
 func (p *Pcap) NextEx() (pkt *Packet, result int32) {
-	var pkthdr_ptr *C.struct_pcap_pkthdr
-	var pkthdr C.struct_pcap_pkthdr
+	var pkthdr *C.struct_pcap_pkthdr
 
 	var buf_ptr *C.u_char
 	var buf unsafe.Pointer
-	result = int32(C.hack_pcap_next_ex(p.cptr, &pkthdr_ptr, &buf_ptr))
+	result = int32(C.hack_pcap_next_ex(p.cptr, &pkthdr, &buf_ptr))
 
 	buf = unsafe.Pointer(buf_ptr)
-	pkthdr = *pkthdr_ptr
 
 	if nil == buf {
 		return
@@ -110,11 +108,7 @@ func (p *Pcap) NextEx() (pkt *Packet, result int32) {
 	pkt.Time = time.Unix(int64(pkthdr.ts.tv_sec), int64(pkthdr.ts.tv_usec))
 	pkt.Caplen = uint32(pkthdr.caplen)
 	pkt.Len = uint32(pkthdr.len)
-	pkt.Data = make([]byte, pkthdr.caplen)
-
-	for i := uint32(0); i < pkt.Caplen; i++ {
-		pkt.Data[i] = *(*byte)(unsafe.Pointer(uintptr(buf) + uintptr(i)))
-	}
+	pkt.Data = C.GoBytes(buf, C.int(pkthdr.caplen))
 	return
 }
 
