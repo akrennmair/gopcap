031ea7c1ed37dae06f73c0fa14d61ae626d13ab3
c9s <cornelius.howl@gmail.com>
2013-01-25 16:25:44 +0800

    porting DLT consts

--- decode.go
+++ decode.go
@@ -29,6 +29,26 @@ const (
 	NSEC_TCPDUMP_MAGIC = 0xa1b23c4d
 )
 
+
+/**
+ * DLT
+ * These are the types that are the same on all platforms, and that
+ * have been defined by <net/bpf.h> for ages.
+ */
+const (
+	DLT_NULL	= 0	    /* BSD loopback encapsulation */
+	DLT_EN10MB	= 1	    /* Ethernet (10Mb) */
+	DLT_EN3MB	= 2	    /* Experimental Ethernet (3Mb) */
+	DLT_AX25	= 3	    /* Amateur Radio AX.25 */
+	DLT_PRONET	= 4	    /* Proteon ProNET Token Ring */
+	DLT_CHAOS	= 5	    /* Chaos */
+	DLT_IEEE802	= 6	    /* 802.5 Token Ring */
+	DLT_ARCNET	= 7	    /* ARCNET, with BSD-style header */
+	DLT_SLIP	= 8	    /* Serial Line IP */
+	DLT_PPP		= 9	    /* Point-to-point Protocol */
+	DLT_FDDI	= 10	/* FDDI */
+)
+
 const (
 	ERRBUF_SIZE = 256
 
